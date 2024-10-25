32b8d73eb23f257a57256001cf41388934d615af
siddu <siddugarikapati@gmail.com>
2024-10-25 18:40:21 +0530

    Delete server folder

--- server/routes/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { signupUser } = require('../controller/user-controller');
-
-const router = express.Router();
-
-router.post('/', signupUser);
-
-module.exports = router;
